c19170567053a3162083d75c38dd84f9b2bab741
MarinaCh165 <marina.marinam48@gmail.com>
2020-06-20 22:27:22 +0545

    Update routes.js

--- server/src/core/routes.js
+++ server/src/core/routes.js
@@ -6,12 +6,15 @@ const { check } = require('express-validator');
 const { updateLastSeen, checkAuth } = require('../middlewares');
 
 const  UserCtrl = require('../controllers/UserController')
-
+const  DialogCtrl = require('../controllers/DialogController')
+const  MessageCtrl = require('../controllers/MessageController')
 
 const createRoutes = (app, io) => {
 
 
   const UserController = new UserCtrl(io);
+  const DialogController = new DialogCtrl(io);
+  const MessageController = new MessageCtrl(io);
 
   app.use(bodyParser.json());
   app.use(checkAuth);
@@ -38,6 +41,13 @@ const createRoutes = (app, io) => {
   app.get("/user/:id", UserController.show);
   app.delete("/user/:id", UserController.delete);
 
+  app.get("/dialogs", DialogController.index);
+  app.delete("/dialogs/:id", DialogController.delete);
+  app.post("/dialogs", DialogController.create);
+
+  app.get("/messages", MessageController.index);
+  app.post("/messages", MessageController.create);
+  app.delete("/messages", MessageController.delete);
 };
 
 module.exports = createRoutes;
