3c7e59816b2d568d6ee3436f009a65d944330498
MarinaCh165 <marina.marinam48@gmail.com>
2020-06-20 22:17:27 +0545

    Add files via upload

---
+++ server/src/utils/index.js
@@ -0,0 +1,6 @@
+const createJWToken = require('./createJWToken');
+const verifyJWTToken = require('./verifyJWTToken');
+const generatePasswordHash = require('./generatePasswordHash');
+
+module.exports = {createJWToken, verifyJWTToken, generatePasswordHash}
+
